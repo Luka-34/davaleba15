a8a18571c7ede2a5edf808a0d5ee2667eabbb9f7
Luka-34 <lchikhladze960@gmail.com>
2024-07-06 17:43:14 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import "./App.css"
+import useLocalStorage from './hooks/useLocalStorage'
+import useWindowResize from './hooks/useWindowsResize'
+
+
+const App = () => {
+    const [color, setColor] = useLocalStorage('tab', false)
+    const {width, height} = useWindowResize()
+
+   
+  const ChangeColor = () => {
+    if (width < 1200) {
+      setColor('white');
+    } else {
+      setColor(prevColor => (prevColor === 'white' ? 'black' : 'white'));
+    }
+  };
+
+
+    useEffect(() => {
+        document.body.style.backgroundColor = color
+    }, [color])
+
+  return (
+         <div className='btn-box'>         
+         <button className='btn' onClick={ChangeColor}>Change Theme</button>    
+         </div>
+  )
+}
+
+export default App
